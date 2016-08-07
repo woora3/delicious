446a04687974795026bf13e22d386144a02a855b
woora3 <woora3@gmail.com>
2016-08-08 00:04:18 +0800

    d4
    d4

--- main.go
+++ main.go
@@ -94,7 +94,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 					address := strings.Join(results.Businesses[i].Location.DisplayAddress,",")
 					_, err = bot.SendImage([]string{content.From}, results.Businesses[i].ImageURL, results.Businesses[i].ImageURL)
 					//SendImage(to []string, imageURL, previewURL string) (result *ResponseContent, err error)
-					bot.NewRichMessage(1040)
+					//bot.NewRichMessage(1040)
 					richbot.SetAction("thisActionName", "this is text", results.Businesses[i].URL)
 					richbot.SetListener("thisActionName", 0, 0, 1040, 1040) 
 					_, err = richbot.Send([]string{content.From}, results.Businesses[i].ImageURL, "this is altText")
