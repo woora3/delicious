1f1cb2d6c3dca5190c255154ca9e830f065a0b6e
woora3 <woora3@gmail.com>
2016-08-13 14:13:55 +0800

    Update main.go

--- main.go
+++ main.go
@@ -16,6 +16,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	//SUrl lib
+	"net/url"
+	"io/ioutil"
+	
 	"os"
 	"strconv"
 	"strings"
@@ -27,6 +31,56 @@ import (
 var bot *linebot.Client
 var richbot *linebot.RichMessageRequest
 var o *yelp.AuthOptions
+
+//shorten url
+const (
+	TINY_URL = 1
+	IS_GD    = 2
+)
+
+type UrlShortener struct {
+	ShortUrl    string
+	OriginalUrl string
+}
+
+func getResponseData(urlOrig string) string {
+	response, err := http.Get(urlOrig)
+	if err != nil {
+		fmt.Print(err)
+	}
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+	return string(contents)
+}
+
+func tinyUrlShortener(urlOrig string) (string, string) {
+	escapedUrl := url.QueryEscape(urlOrig)
+	tinyUrl := fmt.Sprintf("http://tinyurl.com/api-create.php?url=%s", escapedUrl)
+	return getResponseData(tinyUrl), urlOrig
+}
+
+func isGdShortener(urlOrig string) (string, string) {
+	escapedUrl := url.QueryEscape(urlOrig)
+	isGdUrl := fmt.Sprintf("http://is.gd/create.php?url=%s&format=simple", escapedUrl)
+	return getResponseData(isGdUrl), urlOrig
+}
+
+func (u *UrlShortener) short(urlOrig string, shortener int) *UrlShortener {
+	switch shortener {
+	case TINY_URL:
+		shortUrl, originalUrl := tinyUrlShortener(urlOrig)
+		u.ShortUrl = shortUrl
+		u.OriginalUrl = originalUrl
+		return u
+	case IS_GD:
+		shortUrl, originalUrl := isGdShortener(urlOrig)
+		u.ShortUrl = shortUrl
+		u.OriginalUrl = originalUrl
+		return u
+	}
+	return u
+}
+//surl
 
 func main() {
 	strID := os.Getenv("ChannelID")
